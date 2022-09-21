925b7d80b39ca56ef9e694c9300af35208df353f
Bradley van Ewijk <32265034+PrimeLockerbee@users.noreply.github.com>
2022-09-21 19:02:34 +0200

    STATE PATTERN
    Am beginning with adding a state pattern for the spawning system. Trying to see if it wil work with the pool. If not I will add debug.log.

--- Assets/Scripts/Waiting.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Waiting : BaseState
-{
-    public Waiting(StateMachine stateMachine) : base("Waiting", stateMachine) { }
-}
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public int count;
+        public float rate;
+    }
+
+    protected enum SpawnFSM
+    {
+        counting,
+        waiting,
+        spawning
+    }
+
     [SerializeField] GameObject go_SpawnPointRed;
     [SerializeField] GameObject go_SpawnPointBlue;
     [SerializeField] GameObject go_SpawnPointYellow;
 
-    void Update()
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    SpawnFSM spawnMode = SpawnFSM.counting;
+
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SpawnRed();
-        }
+        waveCountdown = timeBetweenWaves;
+    }
+
+    private void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             SpawnBlue();
@@ -23,15 +49,104 @@ public class WaveSpawner : MonoBehaviour
             SpawnYellow();
         }
 
+        DoWave();
+
+        Debug.Log(spawnMode);
     }
 
-    void SpawnRed()
+    protected void DoWave()
+    {
+        switch (spawnMode)
+        {
+            case SpawnFSM.counting:
+                waveCountdown -= Time.deltaTime;
+
+                if (waveCountdown <= 0)
+                {
+                    spawnMode = SpawnFSM.spawning;
+                }
+
+                break;
+            case SpawnFSM.waiting:
+                if(!EnemyIsAlive())
+                {
+                    WaveCompleted();
+                }
+                else
+                {
+                    return;
+                }
+                break;
+            case SpawnFSM.spawning:
+
+                 if(spawnMode == SpawnFSM.spawning)
+                 {
+                    StartCoroutine(SpawnWave(waves[nextWave]));
+                 }
+                break;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+         
+        spawnMode = SpawnFSM.counting;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("All Waves Complete!!");
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+
+        //SEARCHES FOR ALIVE ENEMIES
+        if(searchCountdown <= 0)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("RedEnemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning Wave: " + _wave.name);
+
+        spawnMode = SpawnFSM.spawning;
+
+        //DO SPAWNING
+        for(int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        spawnMode = SpawnFSM.waiting;
+
+        yield break;
+    }
+
+    void SpawnEnemy()
     {
         GameObject EnemyRed = ObjectPooler.SharedInstance.GetPooledObject("RedEnemy");
 
         EnemyRed.transform.position = go_SpawnPointRed.transform.position;
         EnemyRed.transform.rotation = go_SpawnPointRed.transform.rotation;
         EnemyRed.SetActive(true);
+
+        Debug.Log("Spawning Enemy: " + EnemyRed.name);
     }
 
     void SpawnBlue()
@@ -51,5 +166,4 @@ public class WaveSpawner : MonoBehaviour
         EnemyRed.transform.rotation = go_SpawnPointYellow.transform.rotation;
         EnemyRed.SetActive(true);
     }
-
 }
